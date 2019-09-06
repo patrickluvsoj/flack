af3dd182d5095973633e8db5cd77b501438bef92
Patrick Suzuki <hpatricksuzuki@gmail.com>
2019-09-06 09:54:49 -0700

    Added login page

--- application.py
+++ application.py
@@ -1,36 +1,58 @@
 import os
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, request, render_template, session, redirect, url_for
+# from flask_socketio import SocketIO, emit
+from flask_session import Session
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+app.config.from_envvar('YOURAPPLICATION_SETTINGS')
+# Session(app)
+# socketio = SocketIO(app)
 
-""" TODO  
-Define global local storage variables. Current user and current channel
-"""
+#GLOBAL VARIABLES
+user_list = []
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method  == 'POST':
+        username  = request.form.get("username")
+
+        # TODO Remove user user_list when they "leave"
+        for user in user_list:
+            if  username == user:
+                return "Username is already in use. Try another username!"
+        
+        user_list.append(username)
+        session['username'] = username
+        return redirect(url_for("chat"))
+
     return render_template("login.html")
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+    return Chatroom
 
 """ TODO
 #0 Create basic HTML template
-#0.5 Setup dummy channel and message data
-#0.8 Global varariables
+    - Create SCSS file and create container around body
+
+#1 Display Name
+    User sign-up page
+    Check user list
+    Add username to session
+    If not available, store user name
+    Render chat page
+    Add login required decorator
+
+#0.7 Global varariables
     - user list
     - current user
     - messages
 
-#1 Display Name
-    User sign-up page
-    Check local storage for user name
-    If not available, store user name
-    Render chat page
+#.8 Watch Local Storage 
 
 #2 Render channels/chat page
+    - Setup dummy channel and message data
     - Pull channles from messages using Jinja
     - Add create channel button
     - Add leave button
---
+++ config.py
@@ -0,0 +1,4 @@
+# Configurations for  flack
+SECRET_KEY = b'\xe8\x03\x85\xfbY,\x95.UM\x1f\xf1ELx\x02'
+SESSION_PERMANENT =  False
+SESSION_TYPE  =  "file_system"
